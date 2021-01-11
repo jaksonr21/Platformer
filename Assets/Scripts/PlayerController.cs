9046dec8c0494358fc2765a2a141a3b552acd760
Jakson R <818381@mystma.org>
2021-01-11 13:42:05 -0600

    Added Player Movements
    Added Player Movements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpPower;
+
+    Rigidbody2D rb;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpPower);
+        }
+    }
+}
