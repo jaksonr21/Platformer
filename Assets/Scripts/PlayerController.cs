5dd37b385059a48ba10f2297ea9a32d9900cb92f
Jakson R <818381@mystma.org>
2021-01-26 14:13:13 -0600

    Added up to Single Jump
    Added up to Single Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float jumpPower; 
+    public float jumpPower;
 
     Rigidbody2D rb;
 
     public float moveSpeed;
 
+    public float jumpDetectOffset;
+
+    public LayerMask jumpableObjects;
+
     SpriteRenderer sr;
+
+    Vector3 startPos;
+
+    BoxCollider2D bc;
+
 
 
 
@@ -19,12 +28,21 @@ public class PlayerController : MonoBehaviour
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         sr = gameObject.GetComponent<SpriteRenderer>();
+        bc = gameObject.GetComponent<BoxCollider2D>();
+        startPos = transform.position;
     }
+
+        
+      
+
+
+
 
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.Space))
+
+       if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(transform.up * jumpPower);
         }
@@ -55,10 +73,20 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D (Collision2D c)
     {
+      
+        
         if (c.gameObject.tag == "Platform")
         {
             transform.SetParent(c.transform);
         }
+
+        if (c.gameObject.tag == "Respawn")
+        {
+            rb.velocity = Vector3.zero;
+            transform.position = startPos;
+        }
+
+
     }
 
     private void OnCollisionExit2D (Collision2D c)
@@ -68,6 +96,15 @@ public class PlayerController : MonoBehaviour
             transform.SetParent(null);
         }
     }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0f, Vector2.down, jumpDetectOffset, jumpableObjects);
+        Color rayColor = Color.red;
+        Debug.DrawRay(bc.bounds.center - new Vector3(bc.bounds.extents.x, bc.bounds.extents.y + jumpDetectOffset), Vector2.right * bc.bounds.size.x, rayColor);
+        return raycastHit.collider != null;
+    }
+
 
 
 }
